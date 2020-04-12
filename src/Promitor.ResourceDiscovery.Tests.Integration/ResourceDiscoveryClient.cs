476e1102876717d652e6de34e2cc4dbbe7d1ddcd
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-04-12 18:17:37 +0200

    First rough test

---
+++ src/Promitor.ResourceDiscovery.Tests.Integration/ResourceDiscoveryClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Promitor.ResourceDiscovery.Tests.Integration
+{
+    public class ResourceDiscoveryClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public ResourceDiscoveryClient()
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:7777") // TODO: Read from config
+            };
+        }
+
+        public async Task<HttpResponseMessage> GetResourceCollectionsAsync()
+        {
+            return await _httpClient.GetAsync("/api/v1/resources/collections");
+        }
+    }
+}
---
+++ src/Promitor.ResourceDiscovery.Tests.Integration/Services/ResourceCollectionTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Promitor.ResourceDiscovery.Agent.Configuration;
+using Xunit;
+
+namespace Promitor.ResourceDiscovery.Tests.Integration.Services
+{
+    [Category("Integration")]
+    public class ResourceCollectionTests
+    {
+        [Fact]
+        public async Task ResourceCollection_GetAll_ReturnsValidList()
+        {
+            // Arrange
+            var resourceDiscoveryClient = new ResourceDiscoveryClient();
+            
+            // Act
+            var response = await resourceDiscoveryClient.GetResourceCollectionsAsync();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            var rawResponseBody = await response.Content.ReadAsStringAsync();
+            Assert.NotEmpty(rawResponseBody);
+            var resourceCollections = JsonConvert.DeserializeObject<List<ResourceCollection>>(rawResponseBody);
+            Assert.NotNull(resourceCollections);
+            Assert.NotEmpty(resourceCollections);
+        }
+    }
+}
